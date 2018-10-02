c8ae29f4d3538d8ddf2d358d3c69a980775a8482
Graham McCulloch <admin@grahammcculloch.co.uk>
2018-10-02 14:44:48 +0700

    Installed react-navigation and native-base

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './style';
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Live reload!</Text>
+  </View>
+);
+
+export default App;
