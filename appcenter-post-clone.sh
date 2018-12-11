7b9e3fabb2e202bc16739f954edf77f4396ea51c
Graham McCulloch <admin@grahammcculloch.co.uk>
2018-12-11 14:49:01 +0700

    Fixed Sentry config for Android

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,3 +2,6 @@
 
 echo "Creating an .env file"
 echo "ADROIT_GMAPS_API_KEY=${ADROIT_GMAPS_API_KEY}" > .env
+
+echo "Adding the Sentry Auth token"
+echo "auth.token=${SENTRY_AUTH_TOKEN}" >> android/sentry.properties
