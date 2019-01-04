30411e22ddb732aa2eec2df27346cc04ed29714c
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-01-04 09:11:33 +0700

    Write all env variables to .env for App Center

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,6 +2,13 @@
 
 echo "Creating an .env file"
 echo "ADROIT_GMAPS_API_KEY=${ADROIT_GMAPS_API_KEY}" > .env
+echo "APPCENTER_KEYSTORE_PASSWORD=${APPCENTER_KEYSTORE_PASSWORD}" >> .env
+echo "APPCENTER_KEY_ALIAS=${APPCENTER_KEY_ALIAS}" >> .env
+echo "APPCENTER_KEY_PASSWORD=${APPCENTER_KEY_PASSWORD}" >> .env
+echo "SENTRY_ENDPOINT=${SENTRY_ENDPOINT}" >> .env
+echo "COUNTLY_HOST=${COUNTLY_HOST}" >> .env
+echo "COUNTLY_APP_KEY=${COUNTLY_APP_KEY}" >> .env
+echo "FORCE_REMOTE_MONITORING=${FORCE_REMOTE_MONITORING}" >> .env
 
 echo "Adding the Sentry Auth token"
 echo "auth.token=${SENTRY_AUTH_TOKEN}" >> android/sentry.properties
