af49f1059d17c0fd74bdff62a6319dba4f199f24
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-01-08 11:04:28 +0700

    Update photo max height/width

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,8 +1,8 @@
 const Constants = {
   defaultToastDurationMs: 4000,
   networkRequestTimeoutMs: 60000,
-  imageUploadMaxWidth: 1000,
-  imageUploadMaxHeight: 1000,
+  imageUploadMaxWidth: 1024,
+  imageUploadMaxHeight: 1024,
 };
 
 export default Constants;
