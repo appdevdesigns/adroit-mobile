b032cf842ffa9ebe316fbedaf0d615fa6c6cb847
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-01-31 09:44:08 +0200

    Better setting of variables in properties files

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Creating an .env file"
-echo "APPCENTER_KEYSTORE_PASSWORD=${APPCENTER_KEYSTORE_PASSWORD}" >> .env
-echo "APPCENTER_KEY_ALIAS=${APPCENTER_KEY_ALIAS}" >> .env
-echo "APPCENTER_KEY_PASSWORD=${APPCENTER_KEY_PASSWORD}" >> .env
-echo "SENTRY_ENDPOINT=${SENTRY_ENDPOINT}" >> .env
-echo "COUNTLY_HOST=${COUNTLY_HOST}" >> .env
-echo "COUNTLY_APP_KEY=${COUNTLY_APP_KEY}" >> .env
-echo "FORCE_REMOTE_MONITORING=${FORCE_REMOTE_MONITORING}" >> .env
+if [ ! -f .env ]; then
+  echo "Creating an .env file"
+  echo "APPCENTER_KEYSTORE_PASSWORD=${APPCENTER_KEYSTORE_PASSWORD}" >> .env
+  echo "APPCENTER_KEY_ALIAS=${APPCENTER_KEY_ALIAS}" >> .env
+  echo "APPCENTER_KEY_PASSWORD=${APPCENTER_KEY_PASSWORD}" >> .env
+  echo "SENTRY_ENDPOINT=${SENTRY_ENDPOINT}" >> .env
+  echo "COUNTLY_HOST=${COUNTLY_HOST}" >> .env
+  echo "COUNTLY_APP_KEY=${COUNTLY_APP_KEY}" >> .env
+  echo "FORCE_REMOTE_MONITORING=${FORCE_REMOTE_MONITORING}" >> .env
+else
+  echo ".env file already exists - not updated"
+fi
 
-echo "Adding the Sentry Auth token"
-echo "auth.token=${SENTRY_AUTH_TOKEN}" >> android/sentry.properties
-echo "auth.token=${SENTRY_AUTH_TOKEN}" >> ios/sentry.properties
+echo "Setting the Sentry Auth token"
+sed -i "s/<SENTRY_AUTH_TOKEN>/${SENTRY_AUTH_TOKEN}/" android/sentry.properties
+sed -i "s/<SENTRY_AUTH_TOKEN>/${SENTRY_AUTH_TOKEN}/" ios/sentry.properties
+
+echo "Setting gradle.properties entries"
+sed -i "s/<CODE_PUSH_KEY_ANDROID_STAGING>/${CODE_PUSH_KEY_ANDROID_STAGING}/" android/app/gradle.properties
+sed -i "s/<CODE_PUSH_KEY_ANDROID_PRODUCTION>/${CODE_PUSH_KEY_ANDROID_PRODUCTION}/" android/app/gradle.properties
