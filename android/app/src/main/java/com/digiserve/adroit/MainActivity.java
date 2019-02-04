3ab218573276be51d6c1c85868ae842168ebb700
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-02-04 12:31:09 +0200

    Backtracked on custom Android theme

--- android/app/src/main/java/com/digiserve/adroit/MainActivity.java
+++ android/app/src/main/java/com/digiserve/adroit/MainActivity.java
@@ -3,14 +3,14 @@ package com.digiserve.adroit;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
-import android.content.Intent; 
+import android.content.Intent;
 import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
+     * Returns the name of the main component registered from JavaScript. This is
+     * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
@@ -20,7 +20,6 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.show(this);
-        super.setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
     }
 
