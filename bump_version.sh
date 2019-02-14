c5b2c9db391be500b3941b48f8f3692eedaaf60c
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-02-14 20:12:52 +0200

    Closes #13 script for updating version numbers
    Usage: 
    ```
    npm run bump_version -- #.#.#
    ```

---
+++ bump_version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -z $1 ]; then 
+  echo "ERROR: Version number not provided"
+  exit -1
+fi
+
+echo "Updating the version number"
+sed -i'' -e "s/\"version\": \"[0-9]\+.[0-9]\+.[0-9]\+\"/\"version\": \"$1\"/" package.json
+sed -i'' -e "s/versionName \"[0-9]\+.[0-9]\+.[0-9]\+\"/versionName \"$1\"/" android/app/build.gradle
+sed -i'' -e "/<key>CFBundleShortVersionString<\/key>/!b;n;c\\\t<string>$1</string>" ios/adroit/info.plist
+sed -i'' -e "/<key>CFBundleVersion<\/key>/!b;n;c\\\t<string>$1</string>" ios/adroit/info.plist
