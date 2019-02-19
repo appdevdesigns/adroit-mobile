7f3640e12390be7219238f5836b76b66e418dd53
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-02-19 13:50:52 +0200

    Filter by word boundary

--- src/components/common/Select/SelectUtil.js
+++ src/components/common/Select/SelectUtil.js
@@ -1,4 +1,4 @@
 export const filterItems = (filter, allItems, displayKey) => {
-  const filterRegExp = RegExp(filter, 'i');
+  const filterRegExp = RegExp(`\\b${filter}`, 'i');
   return allItems.filter(item => filterRegExp.test(item[displayKey]));
 };
