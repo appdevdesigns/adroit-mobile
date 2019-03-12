ada630ef0cabecb202e7db4aaa91c726bdbe5256
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-03-12 14:01:06 +0200

    Updates to code push script

--- scripts/code_push.js
+++ scripts/code_push.js
@@ -1,19 +1,20 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-console */
 const chalk = require('chalk');
 const { exec } = require('child_process');
 const argv = require('minimist')(process.argv.slice(2));
 
 const EXIT_CODE = {
-  NO_VERSION: 1,
-  RELEASE_FAILED: 2,
-  HISTORY_FAILED: 3,
-  TAG_FAILED: 4,
-  PUSH_FAILED: 5,
+  MISSING_PARAM: 1,
+  INVALID_ENV: 2,
+  INVALID_OS: 3,
+  RELEASE_FAILED: 4,
+  HISTORY_FAILED: 5,
+  TAG_FAILED: 6,
+  PUSH_FAILED: 7,
 };
 
-const os = argv.o || argv.os || 'android';
-const environment = argv.e || argv.env || 'Staging';
-const version = argv.t || argv.target_version;
+const { os, environment, version } = argv;
 const plist = os === 'ios' ? '--plist-file ios/adroit/Info.plist' : '';
 
 const help = `
@@ -21,85 +22,124 @@ Creates a code push release and tags the current git commit with the release num
 
 node ./srcripts/code_push.js
   -h --help
-  -o --os              Either android or ios
-  -t --target-version  The target version number
-  -e --env             Either Staging or Production
+  --os           Either android or ios
+  --version      The target version number
+  --environment  Either Staging or Production
 `;
 
-if (!version) {
-  console.log(chalk.red('Version number not provided'));
-  console.log(help);
-  process.exit(EXIT_CODE.NO_VERSION);
+// ############################################################################
+// Validate the arguments
+
+function required(param, paramName) {
+  if (!paramName) {
+    console.log(chalk.red(`${paramName} not provided`));
+    console.log(help);
+    process.exit(EXIT_CODE.MISSING_PARAM);
+  }
 }
 
-// ############################################################################
-// 1. Create the Code Push release using the AppCenter CLI
+required(version, 'version');
+required(os, 'os');
+required(environment, 'env');
 
-const releaseCmd = `codepush release-react -a DigiServe/adroit-${os} -d ${environment} -t "${version}" ${plist}`;
+if (!['Staging', 'Production'].includes(environment)) {
+  console.log(chalk.red(`env must be either 'Staging' or 'Production'`));
+  console.log(help);
+  process.exit(EXIT_CODE.INVALID_ENV);
+}
 
-console.log(chalk.cyan(releaseCmd));
+if (!['android', 'ios'].includes(os)) {
+  console.log(chalk.red(`os must be either 'android' or 'ios'`));
+  console.log(help);
+  process.exit(EXIT_CODE.INVALID_OS);
+}
 
-exec(releaseCmd, (error, stdout, stderr) => {
-  if (error) {
-    console.log(chalk.red('Failed to create code push release'));
-    console.log(chalk.red(stderr));
-    process.exit(EXIT_CODE.RELEASE_FAILED);
-  }
-});
+/**
+ * Executes a git push
+ */
+function gitPush() {
+  const pushCmd = 'git push';
 
-// ############################################################################
-// 2. Fetch the corresponding Code Push deployment history and get the latest release version number.
+  console.log(chalk.cyan(pushCmd));
 
-let latestCodePushVersion;
-
-const historyCmd = `appcenter codepush deployment history -a DigiServe/adroit-${os} ${environment} --output="json"`;
-
-console.log(chalk.cyan(historyCmd));
-
-exec(historyCmd, (error, stdout, stderr) => {
-  try {
+  exec(pushCmd, (error, stdout, stderr) => {
     if (error) {
-      throw error;
-    }
-    const history = JSON.parse(stdout);
-    // eslint-disable-next-line prefer-destructuring
-    latestCodePushVersion = history[history.length - 1][0];
-  } catch (err) {
-    console.log(
-      chalk.red(
-        'Failed to fetch latest Code Push version. Please manually call the git tag command to tag this commit with the Code Push release version just created.'
-      )
-    );
-    if (stderr) {
+      console.log(chalk.red('Failed to push tag change'));
+      console.log(chalk.red(error));
       console.log(chalk.red(stderr));
+      process.exit(EXIT_CODE.PUSH_FAILED);
     }
-    process.exit(EXIT_CODE.HISTORY_FAILED);
-  }
-});
+  });
+}
 
-// ############################################################################
-// 3. Tag the current git commit with the Code Push release version and push the tag to origin
+/**
+ * Tag the current git commit with the Code Push release version
+ */
+function gitTag(latestCodePushVersion) {
+  const tagCmd = `git tag -a code-push-${latestCodePushVersion}`;
 
-const tagCmd = `git tag -a code-push-${latestCodePushVersion}`;
+  console.log(chalk.cyan(tagCmd));
 
-console.log(chalk.cyan(tagCmd));
+  exec(tagCmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(chalk.red('Failed to create git tag'));
+      console.log(chalk.red(error));
+      console.log(chalk.red(stderr));
+      process.exit(EXIT_CODE.TAG_FAILED);
+    }
+    gitPush();
+  });
+}
 
-exec(tagCmd, (error, stdout, stderr) => {
-  if (error) {
-    console.log(chalk.red('Failed to create git tag'));
-    console.log(chalk.red(stderr));
-    process.exit(EXIT_CODE.TAG_FAILED);
-  }
-});
+/**
+ *  Fetch the corresponding Code Push deployment history and get the latest release version number.
+ */
+function getHistory() {
+  const historyCmd = `appcenter codepush deployment history -a DigiServe/adroit-${os} ${environment} --output="json"`;
 
-const pushCmd = 'git push';
+  console.log(chalk.cyan(historyCmd));
 
-console.log(chalk.cyan(pushCmd));
+  exec(historyCmd, (error, stdout, stderr) => {
+    try {
+      if (error) {
+        throw error;
+      }
+      const history = JSON.parse(stdout);
+      // eslint-disable-next-line prefer-destructuring
+      const latestCodePushVersion = history[history.length - 1][0];
+      gitTag(latestCodePushVersion);
+    } catch (err) {
+      console.log(
+        chalk.red(
+          'Failed to fetch latest Code Push version. Please manually call the git tag command to tag this commit with the Code Push release version just created.'
+        )
+      );
+      console.log(chalk.red(err));
+      if (stderr) {
+        console.log(chalk.red(stderr));
+      }
+      process.exit(EXIT_CODE.HISTORY_FAILED);
+    }
+  });
+}
 
-exec(pushCmd, (error, stdout, stderr) => {
-  if (error) {
-    console.log(chalk.red('Failed to push tag change'));
-    console.log(chalk.red(stderr));
-    process.exit(EXIT_CODE.PUSH_FAILED);
-  }
-});
+/**
+ *  Create the Code Push release using the AppCenter CLI
+ */
+function createCodePushRelease() {
+  const releaseCmd = `appcenter codepush release-react -a DigiServe/adroit-${os} -d ${environment} -t "${version}" ${plist}`;
+
+  console.log(chalk.cyan(releaseCmd));
+
+  exec(releaseCmd, (error, stdout, stderr) => {
+    if (error) {
+      console.log(chalk.red('Failed to create code push release'));
+      console.log(chalk.red(error));
+      console.log(chalk.red(stderr));
+      process.exit(EXIT_CODE.RELEASE_FAILED);
+    }
+    getHistory();
+  });
+}
+
+createCodePushRelease();
