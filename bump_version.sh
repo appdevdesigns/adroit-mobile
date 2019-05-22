28e6922dff518196a151a9a133e893b3427e2d3a
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-05-22 09:37:24 +0700

    FIxed indentation of bump_version script output

--- bump_version.sh
+++ bump_version.sh
@@ -12,5 +12,5 @@ fi
 echo "Updating the version number"
 sed -i'' -e "s/\"version\": \"[0-9]\+.[0-9]\+.[0-9]\+\"/\"version\": \"$1\"/" package.json
 sed -i'' -e "s/versionName \"[0-9]\+.[0-9]\+.[0-9]\+\"/versionName \"$1\"/" android/app/build.gradle
-sed -i'' -e "/<key>CFBundleShortVersionString<\/key>/!b;n;c\\\t<string>$1</string>" ios/adroit/info.plist
-sed -i'' -e "/<key>CFBundleVersion<\/key>/!b;n;c\\\t<string>$1</string>" ios/adroit/info.plist
+sed -i'' -e "/<key>CFBundleShortVersionString<\/key>/!b;n;c\\\t\\t<string>$1</string>" ios/adroit/info.plist
+sed -i'' -e "/<key>CFBundleVersion<\/key>/!b;n;c\\\t\\t<string>$1</string>" ios/adroit/info.plist
