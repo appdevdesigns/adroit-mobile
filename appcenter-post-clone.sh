aff425b7bb53df0c8fa1f14f30972acb5f87a042
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-07-02 12:29:52 +0700

    Repo must be clean when running git-crypt unlock

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-chmod +x ./postinstall.sh
-
 # First install git-crypt and gpg
 brew install git-crypt
 brew install gnupg
@@ -9,3 +7,5 @@ brew install gnupg
 # Then use the GPG key to unlock the repo
 echo "$GPG_KEY" |base64 -D >temp.key 
 git crypt unlock temp.key
+
+chmod +x ./postinstall.sh
