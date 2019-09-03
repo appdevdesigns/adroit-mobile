a74b1d9c018472ae6c0d325e27eeeb7f305d3834
Graham McCulloch <admin@grahammcculloch.co.uk>
2019-09-03 14:11:06 +0700

    #45 Check session after app is foregrounded

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AppState } from 'react-native';
 import { Provider } from 'mobx-react';
 import codePush from 'react-native-code-push';
 import Config from 'react-native-config';
@@ -32,22 +33,52 @@ const getActiveRouteName = navigationState => {
   return route.routeName;
 };
 
-const App = () => (
-  <Root>
-    <StyleProvider style={getTheme(theme)}>
-      <Provider {...store}>
-        <Navigation
-          onNavigationStateChange={(prevState, currentState) => {
-            const currentScreen = getActiveRouteName(currentState);
-            const prevScreen = getActiveRouteName(prevState);
-            if (prevScreen !== currentScreen) {
-              Monitoring.screenView(currentScreen);
-            }
-          }}
-        />
-      </Provider>
-    </StyleProvider>
-  </Root>
-);
+class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appState: AppState.currentState,
+    };
+  }
+
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
+  }
+
+  handleAppStateChange = (nextAppState) => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === 'active'
+    ) {
+      Monitoring.debug('App has come to the foreground - checking session');
+      store.auth.checkSession();
+    }
+    this.setState({appState: nextAppState});
+  };
+
+  render() {
+    return (
+      <Root>
+        <StyleProvider style={getTheme(theme)}>
+          <Provider {...store}>
+            <Navigation
+              onNavigationStateChange={(prevState, currentState) => {
+                const currentScreen = getActiveRouteName(currentState);
+                const prevScreen = getActiveRouteName(prevState);
+                if (prevScreen !== currentScreen) {
+                  Monitoring.screenView(currentScreen);
+                }
+              }}
+            />
+          </Provider>
+        </StyleProvider>
+      </Root>
+    );
+  }
+}
 
 export default codePush(App);
